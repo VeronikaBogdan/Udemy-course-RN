2f06ee6b368837ad21386b12c3f6bb7ce9d8f1f3
Veronika <veron9399bog@gmail.com>
2022-02-01 00:32:03 +0300

    feat: add TextInput and Button with some styles (23)

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View></View>
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder='Course Goal' style={{borderBottomColor: 'black', borderBottomWidth: 1, padding: 10, marginBottom: 5}} />
+        <Button title='ADD' />
+      </View>
       <View></View>
     </View>
   );
