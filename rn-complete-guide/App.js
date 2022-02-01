0037ab88ee94e00133590bec88edc293b068e0df
Veronika <veron9399bog@gmail.com>
2022-02-01 19:51:18 +0300

    feat: styles're moved from inline to StyleSheet

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput
-          placeholder='Course Goal'
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10, marginBottom: 5 }}
-        />
-        <Button title='ADD' />
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="ADD" />
       </View>
       <View></View>
     </View>
@@ -17,5 +14,19 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  root: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+  },
 });
