c84d8eb01eeb89591f080ab9f49ac379e9008eff
Veronika <veron9399bog@gmail.com>
2022-02-02 02:40:46 +0300

    feat: add full (27)

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState(""); // пустая строка, т.к. юзер еще ничего не ввел
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    // setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]); // it's better
   };
 
   return (
     <View style={styles.root}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
+        <TextInput placeholder="Course Goal" onChangeText={goalInputHandler} value={enteredGoal} style={styles.input} />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>;
+      <View>
+
+      </View>
     </View>
   );
 }
@@ -35,7 +39,7 @@ const styles = StyleSheet.create({
   input: {
     width: "80%",
     borderColor: "black",
-    borderWidth: 1,
+    borderBottomWidth: 1,
     padding: 10,
     marginBottom: 5,
   },
