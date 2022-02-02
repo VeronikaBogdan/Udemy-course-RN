887856a91738098aded98254dd49f46d8f55d4a8
Veronika <veron9399bog@gmail.com>
2022-02-02 03:21:49 +0300

    feat: add scrollView (30)

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState(""); // пустая строка, т.к. юзер еще ничего не ввел
@@ -20,13 +20,13 @@ export default function App() {
         <TextInput placeholder="Course Goal" onChangeText={goalInputHandler} value={enteredGoal} style={styles.input} />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => (
           <View key={goal} style={styles.listItem}>
             <Text>{goal}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
     </View>
   );
 }
