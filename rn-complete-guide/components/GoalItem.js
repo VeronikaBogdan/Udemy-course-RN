22461a3538a6cd127a4ccafc5c4fcec9e7a10076
Veronika <veron9399bog@gmail.com>
2022-02-03 03:05:47 +0300

    feat: learn about Tochable.. (34)

--- rn-complete-guide/components/GoalItem.js
+++ rn-complete-guide/components/GoalItem.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, TouchableWithoutFeedback } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <View style={styles.listItem}>
-      <Text>{props.title}</Text>
-    </View>
+		<TouchableNativeFeedback onPress={props.onDelete}>
+			<View style={styles.listItem}>
+				<Text>{props.title}</Text>
+			</View>
+		</TouchableNativeFeedback>
   );
 };
 
