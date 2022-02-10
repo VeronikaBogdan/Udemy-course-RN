6291144adc9cae723b3ab0566171b8583da4a3e5
Veronika <veron9399bog@gmail.com>
2022-02-11 00:23:04 +0300

    feat: finish the game logic

--- rn-number-app/screens/GameOverScreen.js
+++ rn-number-app/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
+      <Text>Number of Rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="new game" onPress={props.onRestart} />
     </View>
   );
 };
@@ -12,6 +15,7 @@ const GameOverScreen = props => {
 const styles = StyleSheet.create({
   screen: {
     //flex: 1, 
+    height: "90%",
     justifyContent: "center",
     alignItems: "center",
   },
