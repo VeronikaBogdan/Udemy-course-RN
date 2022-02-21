ac5f252b90ea9a33932bbe362e6843928cdc6960
Veronika <veron9399bog@gmail.com>
2022-02-22 00:25:14 +0300

    feat: add icons

--- rn-number-app/screens/GameScreen.js
+++ rn-number-app/screens/GameScreen.js
@@ -1,5 +1,6 @@
 import React, {useState, useRef, useEffect} from "react";
-import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons"
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -53,8 +54,8 @@ const GameScreen = props => {
       <Card style={styles.buttonContainer}>
         {/* <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} /> */}
         {/* <Button title="GREATER" onPress={nextGuessHandler.bind(this, "greater")} /> */}
-				<MainButton onPress={nextGuessHandler.bind(this, "lower")}>LOWER</MainButton>			
-				<MainButton onPress={nextGuessHandler.bind(this, "greater")}>GREATER</MainButton>			
+				<MainButton onPress={nextGuessHandler.bind(this, "lower")}><Ionicons name="md-remove" size={24} color="white" /></MainButton>			
+				<MainButton onPress={nextGuessHandler.bind(this, "greater")}><Ionicons name="md-add" size={24} color="white" /></MainButton>			
 			</Card>
     </View>
   );
