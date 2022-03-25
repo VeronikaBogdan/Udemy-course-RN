267576367a081d4cde20e39bcc155c9a4fc84a2b
Veronika <veron9399bog@gmail.com>
2022-03-25 15:08:51 +0300

    feat: do the 1st steps in navigation

---
+++ meals-app/screens/CategoriesScreen.js
@@ -0,0 +1,32 @@
+import { FlatList } from "react-native";
+import CategoryGridTitle from "../components/CategoryGridTitle";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+
+function CategoriesScreen({navigation}) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview');
+    }
+  
+    return (
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPressProp={pressHandler}
+      />
+    );
+  } 
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem.bind()}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
